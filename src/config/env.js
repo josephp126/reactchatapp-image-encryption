40e55ebf9b6491a3062e56289ecf433b0e80ec82
rockstar0711 <happpybopy123@outlook.com>
2021-11-29 00:59:53 +0800

    update

--- src/config/env.js
+++ src/config/env.js
@@ -2,6 +2,8 @@
 
 //const BasicUrl = 'http://192.168.101.21/api';
 
-const BasicUrl = 'http://192.168.101.110:3000/api';
+// const BasicUrl = 'http://192.168.101.110:3000/api';
+
+const BasicUrl = 'http://13.52.238.150:3000/api';
 
 export default BasicUrl;
